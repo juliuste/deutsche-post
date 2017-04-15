a338c31e91a3803aec7d910cb0fba42ac71427ea
Jannis R <mail@jannisr.de>
2017-04-15 17:38:29 +0200

    minor stuff
    - fix traling newlines according to Editorconfig
    - let tests respect the package.json main field
    - let tests fail properly

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const tape = require('tape')
-const post = require('./index')
+const post = require('.')
 
 tape('deutsche-post.municipalities', (t) => {
 	post.municipalities('14057').then((m) => {
@@ -10,5 +10,5 @@ tape('deutsche-post.municipalities', (t) => {
 		t.ok(m[0].municipality==='Berlin', 'municipality municipality')
 		t.ok(m[0].district==='Charlottenburg', 'municipality discrict')
 		t.ok(m[0]['postal-code']==='14057', 'municipality postal-code')
-	})
+	}, t.ifError)
 })
